a03b0cc36b8466cbf9ef322076b2ce91e7778741
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2023-09-05 10:23:37 -0400

    state data

---
+++ 03_states.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(scales)
+
+dat <- read_csv("data/states.csv")
+
+dat |> 
+  ggplot(aes(x = inc_med, y = biden_vshare)) +
+  geom_text(aes(label = st)) +
+  scale_x_continuous(labels = ~ dollar(.x, scale = 0.001, suffix = "k")) +
+  scale_y_continuous(labels = percent) +
+  labs(
+    x = "Median Income",
+    y = "Biden Voteshare"
+  ) +
+  theme(plot.margin = margin(0.2, 0.2, 0.2, 0.2, "in"))
+
+# 3 ----
+ggsave("figures/states.png", width = 5, height = 4.5)
